ceb7de9e030f732be292599c5b55823b58768caf
rzatalibov555 <sethubpro@gmail.com>
2024-11-12 19:16:16 +0400

    Lesson 1

---
+++ index.py
@@ -0,0 +1,74 @@
+# https://www.youtube.com/watch?v=Z1DIwpM5q9A
+
+# x = 5
+# y = "John"
+# print(x)
+# print(y)
+
+
+# x = 4       # x is of type int
+# x = "Sally" # x is now of type str
+# print(x)
+
+
+# x, y, z = "Orange", "Banana", "Cherry"
+# print(x)
+# print(y)
+# print(z)
+
+
+# x = y = z = "Orange"
+# print(x)
+# print(y)
+# print(z)
+
+
+# fruits = ["apple", "banana", "cherry"]
+# x, z = fruits
+
+# print(x)
+
+# print(z)
+
+# x = "Vaqif"
+
+# def fullName():
+#     print(x)
+
+# fullName()
+
+# print(x)
+
+
+# x = {"name" : "John", "age" : 36}
+
+
+x = {"apple", "banana", "cherry"}
+
+# print(x)
+
+# x = frozenset({"apple", "banana", "cherry"})
+# x = False
+# x = b"Hello"
+# x = bytearray(5)
+# x = memoryview(bytes(5))
+x = None
+print(type(x))
+
+
+# x = ["a1", "a2"]
+
+# x = Array("a1", "a2")
+
+# x = ["apple", "banana", "cherry"]
+# x = list(("apple", "banana", "cherry"))
+
+
+# if 1 > 2:
+#     print("Five is greater than two!")
+# elif 6 > 9:
+#     print("Tester")
+# else:
+#     print("sdsdsd")
+#     #print("Hello, World!")
+# print("Cheers, Mate!")
